b9bf063851099b49ac1a061a8055024b56696071
Alexander Liu <liujalexander84@gmail.com>
2021-03-08 11:22:03 -0800

    add a filecache, i think it works lol

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,6 +8,7 @@ import z from "zod";
 import { log } from "./log";
 import { getconfig } from "./config";
 import mimedb from "mime-db";
+import jssha from "jssha";
 
 isMaster ? main() : worker();
 
@@ -32,88 +33,116 @@ async function worker() {
    }) : undefined;
 
    log("up!");
-}
 
-function createHTTPserver(port: number, handler?: (req: http.IncomingMessage, res: http.ServerResponse) => void) {
-   const serv = http.createServer({}, handler ?? requesthandler);
-   serv.listen(port);
-   return serv;
-}
-
-function createHTTPSserver(port: number, opts: {
-   ca: string;
-   cert: string;
-   key: string;
-}, handler?: (req: http2.Http2ServerRequest, res: http2.Http2ServerResponse) => void) {
-   const serv = http2.createSecureServer({
-      allowHTTP1: true,
-      ca: fs.readFileSync(opts.ca),
-      cert: fs.readFileSync(opts.cert),
-      key: fs.readFileSync(opts.key)
-   }, handler ?? requesthandler);
-}
-
-function createHTTPSRedirector(port: number) {
-   return createHTTPserver(port, (req, res) => {
-      if (!req.headers.host || !req.url) return void res.writeHead(400) ?? res.end();
-      res.writeHead(301, { location: `https://${req.headers.host}${req.url}` });
-      res.end();
-   });
-}
-
-function mime(ext: string) {
-   ext.startsWith(".") && (ext = ext.substring(1));
-   const foundmime = Object.entries(mimedb)
-         .find(mime => mime[1].extensions !== undefined && mime[1].extensions.includes(ext));
-   return foundmime ? foundmime[0] : false;
-}
-
-function writetores(res: http2.Http2ServerResponse | http.ServerResponse) {
-   // return function(chunk: any) {
-   //    res instanceof http2.Http2ServerResponse
-   //    ? res.write(chunk)
-   //    : res.write(chunk);
-   // }
-   return res instanceof http2.Http2ServerResponse
-      ? function(chunk: any) {
-         res.write(chunk);
-      } : function(chunk: any) {
-         res.write(chunk);
-      }
-   // typescript you what bruh
-}
-
-const filepath = path.resolve(process.cwd(), "../client/dist");
-const indexfile = fs.readFileSync(path.resolve(filepath, "./index.html"));
-const filecache: { [key: string]: any } = {
-   "/": indexfile
-};
-
-function requesthandler(
-   req: http2.Http2ServerRequest | http.IncomingMessage,
-   res: http2.Http2ServerResponse | http.ServerResponse
-) {
-   let url = req.url;
-   if (!url) url = "/";
-   // console.log(url);
-
-   // try to get a file
-   let file;
-   let mimetype = mime(path.extname(url));
-   let code = 200;
-
-   try {
-      file = fs.readFileSync(path.resolve(filepath, url.startsWith("/") ? url.substring(1) : url));
-   } catch {
-      file = indexfile;
-      mimetype = "text/html";
+   function createHTTPserver(port: number, handler?: (req: http.IncomingMessage, res: http.ServerResponse) => void) {
+      const serv = http.createServer({}, handler ?? requesthandler);
+      serv.listen(port);
+      return serv;
    }
 
-   const write = writetores(res);
+   function createHTTPSserver(port: number, opts: {
+      ca: string;
+      cert: string;
+      key: string;
+   }, handler?: (req: http2.Http2ServerRequest, res: http2.Http2ServerResponse) => void) {
+      const serv = http2.createSecureServer({
+         allowHTTP1: true,
+         ca: fs.readFileSync(opts.ca),
+         cert: fs.readFileSync(opts.cert),
+         key: fs.readFileSync(opts.key)
+      }, handler ?? requesthandler);
+   }
 
-   res.setHeader("content-type", mimetype ? mimetype : "application/octet-stream");
-   res.setHeader("x-content-type-options", "nosniff");
-   res.writeHead(code);
-   write(file);
-   res.end();
+   function createHTTPSRedirector(port: number) {
+      return createHTTPserver(port, (req, res) => {
+         if (!req.headers.host || !req.url) return void res.writeHead(400) ?? res.end();
+         res.writeHead(301, { location: `https://${req.headers.host}${req.url}` });
+         res.end();
+      });
+   }
+
+   function mime(ext: string) {
+      ext.startsWith(".") && (ext = ext.substring(1));
+      const foundmime = Object.entries(mimedb)
+            .find(mime => mime[1].extensions !== undefined && mime[1].extensions.includes(ext));
+      return foundmime ? foundmime[0] : false;
+   }
+
+   function writetores(res: http2.Http2ServerResponse | http.ServerResponse) {
+      // return function(chunk: any) {
+      //    res instanceof http2.Http2ServerResponse
+      //    ? res.write(chunk)
+      //    : res.write(chunk);
+      // }
+      return res instanceof http2.Http2ServerResponse
+         ? function(chunk: any) {
+            res.write(chunk);
+         } : function(chunk: any) {
+            res.write(chunk);
+         }
+      // typescript you what bruh
+   }
+
+   // pagescache: maps paths to hashes
+   // filecache: maps hashes to file buffers
+   // so that you dont accidentally have 4913 copies of index.html stored
+   const pagescache: { [key: string]: string | undefined } = {};
+   const filecache: { [key: string]: [Buffer, string] | undefined } = {};
+
+   const basedir = path.resolve(process.cwd(), "../client/dist");
+
+   function getwithcache(filepath: string): [Buffer, string] {
+      const hash = pagescache[filepath];
+      if (!hash) return getputcache(filepath);
+      let file = filecache[hash];
+      if (file) return file;
+      return getputcache(filepath);
+   }
+
+   function getputcache(filepath: string): [Buffer, string] {
+      let fileisdir = fs.statSync(filepath).isDirectory();
+      const realpath = fileisdir ? path.resolve(filepath, "index.html") : filepath;
+      const file = fs.readFileSync(realpath);
+      let mimetype = mime(path.extname(realpath));
+      mimetype = mimetype ? mimetype : "application/octet-stream";
+
+      const hasher = new jssha("SHA3-512", "ARRAYBUFFER");
+      hasher.update(file);
+
+      const hash = hasher.getHash("B64");
+      pagescache[filepath] = hash;
+      filecache[hash] = [file, mimetype];
+
+      return [file, mimetype];
+   }
+
+   const indexfile = getwithcache(path.resolve(basedir, "./index.html"));
+
+   function requesthandler(
+      req: http2.Http2ServerRequest | http.IncomingMessage,
+      res: http2.Http2ServerResponse | http.ServerResponse
+   ) {
+      let url = req.url;
+      if (!url) url = "/";
+      // console.log(url);
+
+      // try to get a file
+      let file: [Buffer, string];
+      let code = 200;
+
+      try {
+         const filepath = path.resolve(basedir, url.startsWith("/") ? url.substring(1) : url);
+         file = getwithcache(filepath);
+      } catch {
+         file = indexfile;
+      }
+
+      const write = writetores(res);
+
+      res.setHeader("content-type", file[1]);
+      res.setHeader("x-content-type-options", "nosniff");
+      res.writeHead(code);
+      write(file[0]);
+      res.end();
+   }
 }
